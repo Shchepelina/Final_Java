5464a658908e680fc0ee0cc82ee9f627055754be
shchepelina <apt_dobdoctora@mail.ru>
2023-05-01 23:53:39 +0300

    Задание выполнено

---
+++ Toy Store/ToysIsNotCreateException.java
@@ -0,0 +1,19 @@
+public class ToysIsNotCreateException extends Exception{
+  private String info;
+  public ToysIsNotCreateException(int error, String message) {
+      super(message);
+      this.info = String.format("Код ошибки %d", error);
+      if (((error >> 0) & 1) == 1) {
+          this.info += "\r\nНе задано имя игрушки";
+      }
+      if (((error >> 1) & 1) == 1) {
+          this.info += "\r\nНе задано/не корректно задано количество игрушек";
+      }
+      if (((error >> 2) & 1) == 1) {
+          this.info += "\r\nНе задан/не корректно задан шанс розыгрыша игрушки";
+      }
+  }
+
+  public String getInfo() {return info;}
+  
+}
